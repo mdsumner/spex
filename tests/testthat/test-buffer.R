410945d1745d1f68c64aade8f5190672c74cd09a
Michael Sumner <mdsumner@gmail.com>
2017-05-18 00:19:38 +1000

    test coverage

--- tests/testthat/test-buffer.R
+++ tests/testthat/test-buffer.R
@@ -2,4 +2,6 @@ context("buffer")
 
 test_that("whole grain buffers work", {
   expect_identical(buffer_extent(extent(lux), 10) %>% as_double(), c(0, 10, 40, 60))
+  expect_equal(buffer_extent(extent(lux), 10) %>% as_integer(), c(0, 10, 40, 60))
+  
 })
