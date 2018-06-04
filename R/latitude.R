deedd768cec9dcef672b29b0f0704a266a952b87
Michael Sumner <mdsumner@gmail.com>
2018-06-04 17:19:16 +1000

    lat

--- R/latitude.R
+++ R/latitude.R
@@ -13,6 +13,10 @@
 #' @examples
 #' latitudecircle()
 latitudecircle <- function(latitude = 0, proj = "+proj=stere +lon_0=0 +lat_0=-90 +lat_ts=-71 +ellps=WGS84", lonlim = c(-180, 180)) {
+  stopifnot(is.numeric(latitude))
+  if (length(latitude) > 1) warning("ignoring multiple latitude values, using first")
+  latitude <- latitude[1L]
+
   raster::spPolygons(proj4::project(cbind(seq(lonlim[1], lonlim[2], length = 1000), latitude), proj), crs = proj, attr  = data.frame(latitude = latitude))
 }
 
@@ -21,19 +25,22 @@ latitudecircle <- function(latitude = 0, proj = "+proj=stere +lon_0=0 +lat_0=-90
 #' @param x a raster layer
 #' @param latitude maximum latitude  (effectively a minimum latitude if `southern = FALSe`)
 #' @param southern flag for whether south-polar context is used, default is `TRUE`
-#' 
+#' @param trim if `TRUE` runs `raster::trim` on the result, to remove `NA` margin
+#' @param ... ignored currently
 #' @return RasterLayer
 #' @export
 #'  
 #' @examples  
 #' library(raster)
 #' plot(latmask(ice, -60))
-latmask <- function(x, latitude = 0, southern = TRUE) {
+#' plot(latmask(ice, -60, trim = TRUE))
+latmask <- function(x, latitude = 0, southern = TRUE, trim = FALSE, ...) {
   if (raster::isLonLat(x))  {
     xy <- sp::coordinates(x)
     } else   {
       xy <- proj4::ptransform(sp::coordinates(x), raster::projection(x), "+init=epsg:4326")
     }
   if (southern) x[xy[,2] > (latitude * pi/180)] <- NA else x[xy[,2] < (latitude * pi/180)] <- NA
+  if (trim) x <- raster::trim(x)
   x
 }
