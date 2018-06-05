ae67fa2745b69040b12fd34849ec9c5d520afaa1
Michael Sumner <mdsumner@gmail.com>
2018-06-06 00:03:04 +1000

    no epsg

--- R/latitude.R
+++ R/latitude.R
@@ -68,7 +68,7 @@ latmask <- function(x, latitude = 0, southern = TRUE, trim = FALSE, ...) {
   if (raster::isLonLat(x))  {
     xy <- sp::coordinates(x)
     } else   {
-      xy <- proj4::ptransform(sp::coordinates(x), raster::projection(x), "+init=epsg:4326")
+      xy <- proj4::ptransform(sp::coordinates(x), raster::projection(x), "+proj=longlat +datum=WGS84")
     }
   if (southern) x[xy[,2] > (latitude * pi/180)] <- NA else x[xy[,2] < (latitude * pi/180)] <- NA
   if (trim) x <- raster::trim(x)
